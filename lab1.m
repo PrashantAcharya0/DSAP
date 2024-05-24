6d398bde64e6ab7034ae7d134fa976e2512798ba
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-05-24 09:55:18 +0545

    Add files via upload

---
+++ lab1.m
@@ -0,0 +1,32 @@
+clc;
+close all;
+
+A=2;
+F=1;
+t=0:0.01:5;
+y = A*sin(2*pi*F*t);
+
+subplot(2,2,1);
+plot(t,y, 'black');
+xlabel('Time');
+ylabel('Signal');
+title('Sine signal Bct053');
+
+subplot(2,2,3);
+stem(t,y, 'r');
+xlabel('Time');
+ylabel('Signal');
+title('Sine signal Bct053');
+
+z = A*cos(2*pi*F*t);
+subplot(2,2,2);
+plot(t,z, 'red');
+xlabel('Time');
+ylabel('Signal');
+title('Cosine signal Bct053');
+
+subplot(2,2,4);
+stem(t,y, 'b');
+xlabel('Time');
+ylabel('Signal');
+title('Cosine signal Bct053');
