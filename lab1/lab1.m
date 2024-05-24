70b63cea67c467b659a9756e3f601153c64a91e2
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-05-24 09:59:34 +0545

    lab1.m

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -10,23 +10,23 @@ subplot(2,2,1);
 plot(t,y, 'black');
 xlabel('Time');
 ylabel('Signal');
-title('Sine signal Bct053');
+title('Sine signal Bct058');
 
 subplot(2,2,3);
 stem(t,y, 'r');
 xlabel('Time');
 ylabel('Signal');
-title('Sine signal Bct053');
+title('Sine signal Bct058');
 
 z = A*cos(2*pi*F*t);
 subplot(2,2,2);
 plot(t,z, 'red');
 xlabel('Time');
 ylabel('Signal');
-title('Cosine signal Bct053');
+title('Cosine signal Bct058');
 
 subplot(2,2,4);
 stem(t,y, 'b');
 xlabel('Time');
 ylabel('Signal');
-title('Cosine signal Bct053');
+title('Cosine signal Bct058');
